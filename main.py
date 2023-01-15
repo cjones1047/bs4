60bdcc293dc19824f2a16759a39613d04ac257f3
cjones1047 <cjmj1047@gmail.com>
2023-01-15 07:26:35 -0600

    Dummy website html added to website.html; Practice scraping with BeautifulSoup added to main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from bs4 import BeautifulSoup
+# import lxml
 
+with open("website.html", "r") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+all_anchor_tags = soup.find_all(name="a")
+
+for tag in all_anchor_tags:
+    print(tag.get("href"))
+
+heading = soup.find(name="h1", id="name")
+print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+print(section_heading)
+
+section_heading = soup.select(".heading")
+print(section_heading)
+
+profile_pic = soup.select("#profile-pic")
+print(profile_pic)
